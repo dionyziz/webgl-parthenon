fa608a6e81aad49e3567d07064cb881a202a3b0b
Panayiotis D <panos_dan@hotmail.com>
2012-05-31 15:52:01 +0300

    stoixisi3

--- pillar.js
+++ pillar.js
@@ -11,33 +11,33 @@ function pillar( n, h, br, cr, s ) {
     var theta, vertices = [], indices = [], normals = [];
     var step = 2 * Math.PI / n;
     var index = 0;
-	var r, i, r_step = ( cr - br ) / ( s / 2 );
+    var r, i, r_step = ( cr - br ) / ( s / 2 );
 
     r_up = br;
-	r_down = br;
-	for ( i = 0; i < s/2; i += 1 ) {
+    r_down = br;
+    for ( i = 0; i < s/2; i += 1 ) {
         r_up = r_down;
         r_down += r_step;
-		
+            
         for ( theta = 0; theta < 2 * Math.PI; theta += 2 * Math.PI / n ) {
             var a = [ r_up * Math.cos( theta ), h / 2, r_up * Math.sin( theta ) ];
             var b = [ r_down * Math.cos( theta ), 0, r_down * Math.sin( theta ) ];
-			var c = [ r_down * Math.cos( theta + step ), 0, r_down * Math.sin( theta + step ) ];
-			var d = [ r_up * Math.cos( theta + step ), h / 2, r_up * Math.sin( theta + step ) ];
-			vertices.push.apply( vertices, a );
-			vertices.push.apply( vertices, b );
-			vertices.push.apply( vertices, c );
-			vertices.push.apply( vertices, d );
-			var n1 = [ a[ 0 ] / r_up, 0, a[ 2 ] / r_up ];
-			var n2 = [ c[ 0 ] / r_down, 0, c[ 2 ] / r_down ];
-			var n3 = [ c[ 0 ] / r_down, 0, c[ 2 ] / r_down ];
-			var n3 = [ c[ 0 ] / r_up, 0, c[ 2 ] / r_up ];
-			normals.push.apply( normals, n1 );
-			normals.push.apply( normals, n1 );
-			normals.push.apply( normals, n2 );
-			normals.push.apply( normals, n2 );
-			indices.push( index, index + 3, index + 1, index + 2, index + 1, index + 3 );
-			index += 4;
+            var c = [ r_down * Math.cos( theta + step ), 0, r_down * Math.sin( theta + step ) ];
+            var d = [ r_up * Math.cos( theta + step ), h / 2, r_up * Math.sin( theta + step ) ];
+            vertices.push.apply( vertices, a );
+            vertices.push.apply( vertices, b );
+            vertices.push.apply( vertices, c );
+            vertices.push.apply( vertices, d );
+            var n1 = [ a[ 0 ] / r_up, 0, a[ 2 ] / r_up ];
+            var n2 = [ c[ 0 ] / r_down, 0, c[ 2 ] / r_down ];
+            var n3 = [ c[ 0 ] / r_down, 0, c[ 2 ] / r_down ];
+            var n3 = [ c[ 0 ] / r_up, 0, c[ 2 ] / r_up ];
+            normals.push.apply( normals, n1 );
+            normals.push.apply( normals, n1 );
+            normals.push.apply( normals, n2 );
+            normals.push.apply( normals, n2 );
+            indices.push( index, index + 3, index + 1, index + 2, index + 1, index + 3 );
+            index += 4;
         }
     }
 
