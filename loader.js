48fade70003276c14f833ea5cb1c16f938023aff
Dionysis Zindros <dionyziz@gmail.com>
2012-06-01 18:27:31 -0400

    Code organization

--- loader.js
+++ loader.js
@@ -1,6 +1,6 @@
-wget( 'fragment.c', function ( src ) {
+wget( 'shaders/fragment.c', function ( src ) {
     var fragmentShaderSrc = src;
-    wget( 'vertex.c', function ( src ) {
+    wget( 'shaders/vertex.c', function ( src ) {
         var vertexShaderSrc = src;
         var gl = init( document.getElementById( 'canvas' ), fragmentShaderSrc, vertexShaderSrc );
         Renderer.init( gl );
