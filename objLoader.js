256434d64d59c52865d0501241a88521b3203edc
Panayiotis D <panos_dan@hotmail.com>
2012-06-08 00:31:14 +0300

    objLoader fixed. still synchronous.

--- objLoader.js
+++ objLoader.js
@@ -1,10 +1,16 @@
 var objLoader = {
 
-    loadData: function( object ) {
+    loadObj: function( url ) {
         var request = new XMLHttpRequest();
-        request.open( "GET", object, false );
+        request.open( "GET", url, false );
+        /*request.onreadystatechange = function() {
+            if ( request.readyState != 4 ) {
+                return;
+            }
+            return objLoader.handleLoadedObject( request.responseText );
+        }*/
         request.send();
-        return request.responseText;
+        return objLoader.handleLoadedObject( request.responseText );
     },
 
     handleLoadedObject: function( data ) {
@@ -17,22 +23,22 @@ var objLoader = {
             temp = data[ i ].split( " " );
             switch( temp[ 0 ] ) {
                 case "v":
-                    vertices = objLoader.pushData( vertices, temp );
+                    vertices = pushData( vertices, temp, false );
                     break;
                 case "vt":
-                    textures = objLoader.pushData( textures, temp );
+                    textures = pushData( textures, temp, false );
                     break;
                 case "vn":
-                    normals = objLoader.pushData( normals, temp );
+                    normals = pushData( normals, temp, false );
                     break;
                 case "f":
-                    for ( var j = 1; j < temp.length; j++ ) {
-                        temp[ j ] = temp[ j ].split( "/" );
-                        temp[ j ] = temp[ j ].join( " " );
+                    for ( var j = 1; j< temp.length; j++) {
+                        temp[ j ] = temp[ j ].split("/");
+                        temp[ j ] = temp[ j ].join(" ");
                     }
-                    temp = temp.join( " " );
-                    temp = temp.split( " " );
-                    faces = objLoader.pushData( faces, temp );
+                    temp = temp.join(" ");
+                    temp = temp.split(" ");
+                    faces = pushData( faces, temp, true );
                     break;
                 default:
                     break;
@@ -41,6 +47,7 @@ var objLoader = {
 
         result = objLoader.computeArraysFromFaces( vertices, textures, normals, faces );
 
+        console.log('Object loaded successfully');
         return {
             vertices: result.vertices,
             indices: result.indices,
@@ -51,8 +58,34 @@ var objLoader = {
 
     pushData: function( array, data ) {
         var array = array;
-        for ( var i = 1; i < data.length; i++ ) {
-            array.push( parseFloat( data[ i ] ) );
+        if ( !faces ) {
+            for ( var i = 1; i< data.length; i++ ) {
+                array.push( parseFloat( data[ i ] ) );
+            }
+        }
+        else {
+            var num_of_triangles = ( ( data.length - 1 ) / 3 ) - 2;
+            for ( var i = 1; i<= num_of_triangles; i++ ) {
+                var j = i + 1, k = i + 2;
+                {
+                    //vertex #1
+                    array.push( parseFloat( data[ 1 ] ) );
+                    array.push( parseFloat( data[ 2 ] ) );
+                    array.push( parseFloat( data[ 3 ] ) );
+                }
+                {
+                    //vertex #( j = i + 1 )
+                    array.push( parseFloat( data[ ( 3 * j - 2 ) ] ) );
+                    array.push( parseFloat( data[ ( 3 * j - 2 ) + 1 ] ) );
+                    array.push( parseFloat( data[ ( 3 * j - 2 ) + 2 ] ) );
+                }
+                {
+                    //vertex #( k = i + 2 )
+                    array.push( parseFloat( data[ ( 3 * k - 2 ) ] ) );
+                    array.push( parseFloat( data[ ( 3 * k - 2 ) + 1 ] ) );
+                    array.push( parseFloat( data[ ( 3 * k - 2 ) + 2 ] ) );
+                }
+            }
         }
         return array;
     },
@@ -62,18 +95,18 @@ var objLoader = {
         var vertex, texture, normal;
 
         for ( var i = 0; i < faces.length; i += 3 ) {
-            vertex = 3 * faces [ i ];
+            vertex = 3 * ( faces [ i ] - 1 );
             real_vertices.push( vertices[ vertex ] );
             real_vertices.push( vertices[ vertex + 1 ] );
             real_vertices.push( vertices[ vertex + 2 ] );
-
+            
             real_indices.push( i / 3 );
-
-            texture = 2 * faces [ i + 1 ];
+            
+            texture = 2 * ( faces [ i + 1 ] - 1 );
             real_textures.push( textures[ texture ] );
             real_textures.push( textures[ texture + 1 ] );
-
-            normal = 3 * faces [ i + 2 ];
+            
+            normal = 3 * ( faces [ i + 2 ] - 1 );
             real_normals.push( normals[ normal ] );
             real_normals.push( normals[ normal + 1 ] );
             real_normals.push( normals[ normal + 2 ] );
